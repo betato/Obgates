11596a319e4fb42f3ad1a8ce367e363ca0e2c1ef
Betato <BattlestarGatticus@gmail.com>
2016-04-03 18:06:15 -0400

    Essential gates (and & or)

---
+++ Obgates/NotGate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obgates
+{
+    class NotGate : Component
+    {
+        public NotGate()
+        {
+            pinStates.Add(false);
+            pinStates.Add(false);
+        }
+
+        public override void Step()
+        {
+            pinStates[1] = !pinStates[0];
+        }
+    }
+}
