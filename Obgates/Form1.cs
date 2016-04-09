a5bbdec7137bdd6ec54d6b2d2070b5ef58e7e56c
Betato <BattlestarGatticus@gmail.com>
2016-04-08 23:32:34 -0400

    Moved refresh loop directly into form

--- Obgates/Form1.cs
+++ Obgates/Form1.cs
@@ -10,27 +10,82 @@ using System.Windows.Forms;
 
 namespace Obgates
 {
-    public partial class Form1 : RLoopForm
+    public partial class Form1 : Form
     {
-        public Form1() : base(60, 20)
+        public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void StartRefreshLoop(int targetFps, int targetUps)
+        {
+            int targetFpsNano = 1000000000 / targetFps;
+            int targetUpsNano = 1000000000 / targetUps;
+            NanoTime tm = new NanoTime();
+
+            long startTime = tm.Time();
+
+            long deltaTime = 0;
+            long deltaFps = 0;
+            long deltaUps = 0;
+            long deltaFpsDisplay = 0;
+            int framecount = 0;
+            int updatecount = 0;
+
+            while (true)
+            {
+                // Get current time
+                long currentTime = tm.Time();
+                // Get time since last loop
+                deltaTime = currentTime - startTime;
+                deltaFps += deltaTime;
+                deltaUps += deltaTime;
+                deltaFpsDisplay += deltaTime;
+                // Set start time of this loop for use in next cycle
+                startTime = currentTime;
+
+                // Render
+                if (deltaFps >= targetFpsNano)
+                {
+                    RenderR();
+                    framecount++;
+                    deltaFps = 0;
+                }
+
+                // Update
+                if (deltaUps >= targetUpsNano)
+                {
+                    UpdateR();
+                    updatecount++;
+                    deltaUps = 0;
+                }
+
+                // Update fps display
+                if (deltaFpsDisplay >= 1000000000)
+                {
+                    Console.WriteLine(String.Format("Fps:{0} Ups:{1}", framecount, updatecount));
+                    framecount = 0;
+                    updatecount = 0;
+                    deltaFpsDisplay = 0;
+                }
+            }
+        }
+
+        private void UpdateR()
+        {
+            // Update components
+        }
+
+        private void RenderR()
+        {
+            // Draw frame
         }
 
         Component sim;
 
-        public override void UpdateR()
-        {
-            
-        }
-
-        public override void RenderR()
-        {
-            
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
+            StartRefreshLoop(60, 20);
             // Init gates
             NullGate r1 = new NullGate();
             NullGate r2 = new NullGate();
